979ed81644579584ed7bd55c96b9160828d8e587
Lindsay Ward <lward23@hotmail.com>
2023-06-28 22:22:34 -0700

    Add sessions controller for user login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-  get 'users/create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   
