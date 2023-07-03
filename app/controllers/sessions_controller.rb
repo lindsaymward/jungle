610fd009bf7eb2d834c1798d12ce664d72e83790
Lindsay Ward <lward23@hotmail.com>
2023-07-03 16:55:51 -0700

    Fix login/register authentication and redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-
-    if user && user.authenticate(params[:password])
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
-      render '/login'
+      redirect_to '/login'
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
 
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
   
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
