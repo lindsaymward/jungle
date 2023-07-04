61096e1217e25b170c6d14a012311af48680aaaf
Lindsay Ward <lward23@hotmail.com>
2023-07-03 17:21:54 -0700

    Add tests for authenticating login credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.strip.downcase)
     if user && user.authenticate(password)
       return user
     else
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe 'Validation' do
+  describe 'Validations' do
     it "is valid with email, first_name, last_name, and password fields" do
       @user = User.new(
         {first_name: "Lindsay", 
@@ -38,23 +38,96 @@ RSpec.describe User, type: :model do
       )
       expect(@user).to_not be_valid
     end
+
+    describe '#email' do
+      it "is not valid without an email" do
+        @user = User.new(
+          {first_name: "Lindsay", 
+          last_name: "Ward", 
+          email: nil,
+          password: "password",
+          password_confirmation: "password"
+          }
+        )
+        expect(@user).to_not be_valid
+      end
+
+      it "is not valid without a unique email" do
+        @user1 = User.new(
+          {first_name: "Lindsay", 
+          last_name: "Ward", 
+          email: "lward2391@gmail.com",
+          password: "password",
+          password_confirmation: "password"
+          }
+        )
+        @user1.save
+        @user2 = User.new(
+          {first_name: "Steven",
+          last_name: "Ward",
+          email: "lward2391@gmail.com",
+          password: "password",
+          password_confirmation: "password"
+          }
+        )
+        expect(@user2).to_not be_valid
+      end
+    end
+
+    describe '#password' do
+      it "is not valid without a password" do
+        @user = User.new(
+          {first_name: "Lindsay", 
+          last_name: "Ward", 
+          email: "lward2391@gmail.com",
+          password: nil,
+          password_confirmation: nil
+          }
+        )
+        expect(@user).to_not be_valid
+      end
+
+      it "is not valid without a password confirmation" do
+        @user = User.new(
+          {first_name: "Lindsay", 
+          last_name: "Ward", 
+          email: "lward2391@gmail.com",
+          password: "password",
+          password_confirmation: nil
+          }
+        )
+        expect(@user).to_not be_valid
+      end
+
+      it "is not valid with mis-matching password fields" do
+        @user = User.new(
+          {first_name: "Lindsay", 
+          last_name: "Ward", 
+          email: "lward2391@gmail.com",
+          password: "password",
+          password_confirmation: "password1"
+          }
+        )
+        expect(@user).to_not be_valid
+      end
+      
+      it "has a password with a minimum length of 8 characters" do
+        @user = User.new(
+          {first_name: "Lindsay", 
+          last_name: "Ward", 
+          email: "lward2391@gmail.com",
+          password: "passwor",
+          password_confirmation: "passwor"
+          }
+        )
+        expect(@user).to_not be_valid
+      end
+    end
   end
 
-  describe '#email' do
-    it "is not valid without an email" do
-      @user = User.new(
-        {first_name: "Lindsay", 
-        last_name: "Ward", 
-        email: nil,
-        password: "password",
-        password_confirmation: "password"
-        }
-      )
-      expect(@user).to_not be_valid
-    end
-
-    it "is not valid without a unique email" do
-      @user1 = User.new(
+  describe '.authenticate_with_credentials' do
+    it "is nil if given incorrect password" do
+      user = User.new(
         {first_name: "Lindsay", 
         last_name: "Ward", 
         email: "lward2391@gmail.com",
@@ -62,66 +135,34 @@ RSpec.describe User, type: :model do
         password_confirmation: "password"
         }
       )
-      @user1.save
-      @user2 = User.new(
-        {first_name: "Steven",
-        last_name: "Ward",
+      result = User.authenticate_with_credentials('lward2391@gmail.com', 'password1')
+      expect(result).to eq(nil)
+    end
+
+    it "is still valid if given spaces around email" do
+      user = User.create(
+        {first_name: "Lindsay", 
+        last_name: "Ward", 
         email: "lward2391@gmail.com",
         password: "password",
         password_confirmation: "password"
         }
       )
-      expect(@user2).to_not be_valid
-    end
-  end
-
-  describe '#password' do
-    it "is not valid without a password" do
-      @user = User.new(
-        {first_name: "Lindsay", 
-        last_name: "Ward", 
-        email: "lward2391@gmail.com",
-        password: nil,
-        password_confirmation: nil
-        }
-      )
-      expect(@user).to_not be_valid
+      result = User.authenticate_with_credentials('  lward2391@gmail.com   ', 'password')
+      expect(result).to eq(user)
     end
 
-    it "is not valid without a password confirmation" do
-      @user = User.new(
+    it "is still valid if given wrong cases for email" do
+      user = User.create(
         {first_name: "Lindsay", 
         last_name: "Ward", 
         email: "lward2391@gmail.com",
         password: "password",
-        password_confirmation: nil
+        password_confirmation: "password"
         }
       )
-      expect(@user).to_not be_valid
-    end
-
-    it "is not valid with mis-matching password fields" do
-      @user = User.new(
-        {first_name: "Lindsay", 
-        last_name: "Ward", 
-        email: "lward2391@gmail.com",
-        password: "password",
-        password_confirmation: "password1"
-        }
-      )
-      expect(@user).to_not be_valid
-    end
-    
-    it "has a password with a minimum length of 8 characters" do
-      @user = User.new(
-        {first_name: "Lindsay", 
-        last_name: "Ward", 
-        email: "lward2391@gmail.com",
-        password: "passwor",
-        password_confirmation: "passwor"
-        }
-      )
-      expect(@user).to_not be_valid
+      result = User.authenticate_with_credentials('  lwaRD2391@gmaIl.com   ', 'password')
+      expect(result).to eq(user)
     end
   end
 end
