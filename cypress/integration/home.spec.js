8b7e4ddf82adc4bf3d7eba455337f267b0c0416f
Lindsay Ward <lward23@hotmail.com>
2023-07-03 22:32:16 -0700

    Write another test for home page and create test data for Cypress initializer

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,7 +4,11 @@ describe('Jungle Rails App', () => {
   })
 
   it("There is products on the page", () => {
-    cy.get(".products article").should("be.visible");
+    cy.get(".products > article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products > article").should("have.length", 2);
   });
 })
 
