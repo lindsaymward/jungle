45d1e3d0fb49a94b5c5d5546f0c754589ce35b06
Lindsay Ward <lward23@hotmail.com>
2023-07-03 22:13:17 -0700

    Visit homepage and write first Cypress test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,10 @@
+describe('Jungle Rails App', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+})
+
